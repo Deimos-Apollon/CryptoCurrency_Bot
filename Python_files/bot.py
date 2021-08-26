be903ad6d8b6903f468e23b38a55c79e2c3bd9b1
deimos_apollon <dima31120251@gmail.com>
2021-08-26 16:53:06 +0300

    Added main.py to run bot_start() Refactored bot.py, it is not run module anymore

--- Python_files/bot.py
+++ Python_files/bot.py
@@ -5,8 +5,5 @@ from config import *
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from sql_commands import *
 
-if __name__ == "__main__":
-    print(sql_get_users())
-    print(sql_get_currencies())
-    print(sql_get_user_has_currencies())
-    print(sql_get_user_has_currencies_raw())
+def bot_start():
+    pass
---
+++ Python_files/main.py
@@ -0,0 +1,6 @@
+from bot import bot_start
+
+
+if __name__ == "__main__":
+    bot_start()
+    pass
