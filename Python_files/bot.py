5e7338a465107456eb6b21524d8bdf0696c24833
Roman Cherepanov <romacherepanov2002@gmail.com>
2021-08-29 21:27:19 +0300

    bot.py functions

--- Python_files/bot.py
+++ Python_files/bot.py
@@ -1,9 +1,23 @@
 import json
 import vk_api
 import requests
+from commands import *
 from config import *
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.longpoll import VkLongPoll, VkEventType
 from sql_commands import *
 
+
+def bot_listen(longpoll, vk, users, currencies):
+    for event in longpoll.listen():
+        if event.type == VkEventType.message_new:
+            if event.text == r'\start':
+                add_user(event.from_id)
+
+
 def bot_start():
-    pass
+    vk_session = vk_api.VkApi(token=SERVER_TOKEN)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+    users = sql_get_users()
+    currencies = sql_get_currencies()
+    bot_listen(longpoll, vk, users, currencies)
